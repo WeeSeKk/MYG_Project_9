ee5298303ba21f466149e80e13a2634b326e373c
WeeSeK <113300702+WeeSeKk@users.noreply.github.com>
2025-01-20 17:24:43 +0100

    .

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -43,7 +43,7 @@ namespace Database
         {
             try
             {
-                string connectionString = "mongodb+srv://weesekk:2zmqnh0FQW74CnPd@cluster0.5zx1h.mongodb.net/MYG_Project_9?authMechanism=SCRAM-SHA-1&retryWrites=true&w=majority";
+                string connectionString = "mongodb://weesekk:2zmqnh0FQW74CnPd@cluster0-shard-00-00.5zx1h.mongodb.net:27017,cluster0-shard-00-01.5zx1h.mongodb.net:27017,cluster0-shard-00-02.5zx1h.mongodb.net:27017/?replicaSet=atlas-cvvjrc-shard-0&authSource=admin&retryWrites=true&w=majority&tls=true";
                 client = new MongoClient(connectionString);
                 database = client.GetDatabase("MYG_Project_9");
 
--- Assets/Scripts/Tests/NewTestScript.cs
+++ Assets/Scripts/Tests/NewTestScript.cs
@@ -50,7 +50,6 @@ public class NewTestScript
     [UnityTest]
     public IEnumerator _04GetDatasSQL()
     {
-        yield return SceneManager.LoadSceneAsync(1);
         yield return SceneManager.LoadSceneAsync(0);
         yield return new WaitForSeconds(1);
 
@@ -64,20 +63,4 @@ public class NewTestScript
         Assert.IsTrue(DatabaseManager.instance.currentUsername == "Test");
         Assert.IsNotNull(IHMManager.instance.leaderboardUserdata.usernames);
     }
-/*
-    [UnityTest]
-    public IEnumerator _05SendDatasSQL()
-    {
-        LevelManager levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
-
-        IHMManager.instance.ShowLeaderboard();
-        IHMManager.instance.ShowScoreCount();
-        levelManager.StartGame();
-
-        Time.timeScale = 10;
-        yield return new WaitForSeconds(5);
-        Time.timeScale = 1;
-
-        LogAssert.Expect(LogType.Log, "Score in database is higher or equal");
-    }*/
 }
